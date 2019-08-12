e4f1b4c4e89fe9edb35883681c5d4c3e775195ec
Alexandr143 <you@example.com>
2019-08-13 00:24:20 +0300

    secoted commit

---
+++ task#3/additionaly/task#1/js/index.js
@@ -0,0 +1,69 @@
+`user strict`
+
+// /*
+//   Напиши функцию checkNumberType(num)
+  
+//   Функция получает число num как аргумент и возвращает 
+//   строку "Even" если число четное и строку "Odd" если не четное.
+// */
+
+// const checkNumberType = function (num){
+
+// }
+
+
+
+// console.log( checkNumberType(2) ); // 'Even'
+
+// console.log( checkNumberType(46) ); // 'Even'
+
+// console.log( checkNumberType(3) ); // 'Odd'
+
+// console.log( checkNumberType(17) ); // 'Odd'
+
+// const multiply = function (x, y) {
+//     console.log('x: ', x);
+//     console.log ('y: ', y);
+//     console.log('inside multiply fn');
+//     const result = x * y;
+//     return result;
+// }
+
+// console.log(multiply);
+
+// const z = multiply(5,20);
+
+// console.log(z);
+
+// const findNumbers = function(arr, value){
+//   const numbers =[];
+//   for(let item of arr){
+//       if (item<value){
+//           numbers.push(item);
+//       }
+//   }
+//   return numbers;
+// };
+
+// const arr1 =[12, 5, 46, 78];
+// // const newArr1 = [];
+// // for (let item of arr1) {
+// //   if (item>15){
+// //       newArr1.push(item);
+// //   }
+// // }
+// // console.log (newArr1);
+// const newArr1 = findNumbers(arr1,15);
+// console.log(newArr1);
+
+// const arr2 =[17, 5, 46, 78, 890];
+// const newArr2 = findNumbers(arr2,25);
+// console.log (newArr2);
+// // const  newArr2 = [];
+// // for (let item of arr2){
+// //     if(item>25){
+// //         newArr2.push(item);
+// //     }
+// // }
+
+// // console.log (newArr2);
