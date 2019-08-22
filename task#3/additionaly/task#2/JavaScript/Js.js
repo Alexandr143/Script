8f3938f5af180bc20caf72f4a7a21cecd497cd35
Alexandr143 <you@example.com>
2019-08-22 22:08:13 +0300

    commit

---
+++ task#3/additionaly/task#2/JavaScript/Js.js
@@ -0,0 +1,27 @@
+`use strict`
+/*
+  Напиши функцию checkNumberType(num)
+  
+  Функция получает число num как аргумент и возвращает 
+  строку "Even" если число четное и строку "Odd" если не четное.
+*/
+
+// Вызовы функции для проверки
+
+
+
+const checkNumberType = function (num){
+        if ( num % 2){
+           return 'Odd';
+        } else {
+           return 'Even';
+        };   
+};
+
+console.log( checkNumberType(2) ); // 'Even'
+
+console.log( checkNumberType(46) ); // 'Even'
+
+console.log( checkNumberType(3) ); // 'Odd'
+
+console.log( checkNumberType(17) ); // 'Odd'
