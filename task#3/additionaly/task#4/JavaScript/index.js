8891851d45fe400f42bd0156dd3b1e1dbea3b0a4
Alexandr143 <you@example.com>
2019-08-24 03:18:36 +0300

    commit

--- task#3/additionaly/task#4/JavaScript/index.js
+++ task#3/additionaly/task#4/JavaScript/index.js
@@ -16,9 +16,9 @@
 
 
 const checkForSpam= function (str){
-      let arrStr = str.split(' ');
+      let arrStr = str.split(/\b/);
       for (let i=0;i < arrStr.length;i++){
-          let word =arrStr[i];
+          let word =arrStr[i] .toLowerCase();
           if(word === 'spam' || word ==='sale'){
               return true;
           }
