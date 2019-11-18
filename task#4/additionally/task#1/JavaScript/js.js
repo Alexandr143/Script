f95538c098c5670f81011736583348437f26b49b
Alexandr143 <you@example.com>
2019-11-18 02:37:17 +0200

    commit

---
+++ task#4/additionally/task#1/JavaScript/js.js
@@ -0,0 +1,19 @@
+`use strict`;
+
+// const  user = {
+//     name:'Mango',
+//     age: 10,
+//     friends:300,
+//     status: 'happy',
+//     isActive: true,
+// };
+
+// // console.log(user.age);
+
+// const getPropValue = (obj, prop) => {
+//    console.log (obj  [prop]);
+
+// };
+
+// getPropValue(user, 'age');
+
