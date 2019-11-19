40e9f1d313838c1454535871e059066539c5b463
Alexandr143 <you@example.com>
2019-11-19 02:10:27 +0200

    commit

--- task#4/additionally/task#1/JavaScript/js.js
+++ task#4/additionally/task#1/JavaScript/js.js
@@ -185,64 +185,79 @@
 
 //===========================
 
-const  shop = {
-    products: [
-        {
-        name: 'apples', 
-        amount: 100,
-        price: 30,
-    }, 
-    {
-        name: 'grapes',
-        amount: 150,
-        price: 40,
-    }, 
-    {
-        name: 'bananas', 
-        amount: 200,
-        price: 50,
-    },
-],
-addProduct(product){
-     shop.products.push(product);
-   },
-   getProductInfo(productName){
-       for(const elem of shop.products){
-           if(elem.name === productName){
-              return elem;
-           };
-       };
-   },
-   addProductAmount(productName, num){
-       console.log(`Adding ${num} ${productName}`);       
-         for(const elem of shop.products) {
-             if (elem.name === productName){
-                 elem.amount += num;
-             };
-         };
-   },
-};
+// const  shop = {
+//     products: [
+//         {
+//         name: 'apples', 
+//         amount: 100,
+//         price: 30,
+//     }, 
+//     {
+//         name: 'grapes',
+//         amount: 150,
+//         price: 40,
+//     }, 
+//     {
+//         name: 'bananas', 
+//         amount: 200,
+//         price: 50,
+//     },
+// ],
+//    addProduct(product){
+//      shop.products.push(product);
+//    },
+//    getProductInfo(productName){
+//        for(const elem of shop.products){
+//            if(elem.name === productName){
+//               return elem;
+//            };
+//        };
+//    },
+//    addProductAmount(productName, num){
+//        console.log(`Adding ${num} ${productName}`);
+       
+//        const product = shop.getProductInfo(productName);
 
-// const product = {
+//        product.amount += num;
+
+//         //  for(const elem of shop.products) {
+//         //      if (elem.name === productName){
+//         //          elem.amount += num;
+//         //      };
+//         //  };
+//    },
+//    subtractProductAmount(productName, num) {
+//        console.log(`Selling ${num} ${productName}`);
+//        for (const elem of shop.products){
+//            if (elem.name === productName){
+//                elem.amount -= num;
+//            }
+//        };
+//    }
+// };
+
+// // const product = {
+       
+// //     name: 'mango',
+// //     amount: 300,
+// //     price: 100,
+// // };
+
+// // shop.addProduct(product);
+
+// shop.addProduct({
        
 //     name: 'mango',
 //     amount: 300,
 //     price: 100,
-// };
 
-// shop.addProduct(product);
+// });
 
-shop.addProduct({
-       
-    name: 'mango',
-    amount: 300,
-    price: 100,
+// // console.log(shop);
+// // const info = shop.getProductInfo ('mango');
+// // console.log(info);
 
-});
-
+// shop.addProductAmount('grapes', 150);
+// shop.subtractProductAmount('bananas', 50);
 // console.log(shop);
-// const info = shop.getProductInfo ('mango');
-// console.log(info);
 
-shop.addProductAmount('grapes', 150);
-console.log(shop);
