4c48b0cb11494cd1677df06ea2c35d76f12f4e6f
oleksanderskapa@gmail.com <you@example.com>
2020-04-10 22:44:23 +0300

    commit

---
+++ task#5/additionally/task#1/javaScript/videoTask.js
@@ -0,0 +1,53 @@
+'use strict';
+
+//====================== webinar Repeta ========
+
+//===================== call back function =====
+
+// const hotel = {
+//     name: 'Resort Hotel',
+//     showThis(){
+//         console.log('this в callback : ', this);
+//     },
+// };
+
+// const fn =  function (callback) {
+//     console.log('this в fn ', this);
+
+//     callback();
+// }; 
+
+// fn (hotel.showThis.bind(hotel));
+
+//====================== kit ====================
+
+// const filter = function(array, test) {
+//     const filteredElements = [];
+  
+//     for (const element of array) {
+//       const passed = test(element);
+  
+//       if (passed) {
+//         filteredElements.push(element);
+//       }
+//     }
+  
+//     return filteredElements;
+//   };
+  
+//   const fruits = [
+//     { name: 'apples', quantity: 200, isFresh: true },
+//     { name: 'grapes', quantity: 150, isFresh: false },
+//     { name: 'bananas', quantity: 100, isFresh: true },
+//   ];
+  
+//   const freshFruits = filter(fruits, fruit => fruit.isFresh);
+//   console.dir(freshFruits); // массив с объектами apples и bananas
+  
+//   const fruitsWithQuantity = filter(fruits, fruit => fruit.quantity >= 120);
+//   console.dir(fruitsWithQuantity); // массив с объектами apples и grapes
+
+
+
+  
+
