db12d9b9aadd81d8021b08fc9e015c4519debca0
oleksanderskapa@gmail.com <you@example.com>
2020-05-04 00:58:15 +0300

    commit

---
+++ task#1/hometask new1/javaScript/task4.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// На счету пользователя есть 23580 кредитов, значение хранится в переменной credits (создай и присвой). Пользователь решает купить ремонтных дроидов, которые стоят по 3000 кредитов за штуку. Цена одного дроида хранится в переменной pricePerDroid (создай и присвой).
+
+// При посещении страницы, используя prompt, необходимо спросить количество дроидов которые пользователь хочет купить и сохранить в переменную.
+
+// Напиши скрипт который:
+
+// Если в prompt была нажата кнопка Cancel, выводит в консоль сообщение 'Отменено пользователем!'.
+// В противном случае, рассчитывает общую цену заказа и сохраняет в переменной totalPrice.
+// Проверяет сможет ли пользователь оплатить заказ:
+// если сумма к оплате превышает количество кредитов на счету, выводи в консоль сообщение 'Недостаточно средств на счету!'.
+// в противном случае необходимо посчитать остаток кредитов на счету и вывести сообщение 'Вы купили [число] дроидов, на счету осталось [число] кредитов.'.
+
+let credits = 23580;
+
+let pricePerDroid = 3000; 
+
+let totalPrice = 0;
+
+let lastAmountCredits = 0;
+
+let  input = prompt('How many droits would like you buy ?');
+
+totalPrice += Number(input) * pricePerDroid;
+
+if (input === null) {
+    console.log('User cancel');
+  } else if (totalPrice > credits) {
+    console.log('Not enough resources');
+  } else {
+    lastAmountCredits = credits - totalPrice;
+    console.log(`You bought ${input} droits, on your credit account is lefted ${lastAmountCredits} credits`)
+  }
+
